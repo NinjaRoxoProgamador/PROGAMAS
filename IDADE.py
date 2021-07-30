ca7ce607dea8cbe0eadd60c27c3a04f06db77ed2
NinjaRoxoProgamador <87674724+NinjaRoxoProgamador@users.noreply.github.com>
2021-07-30 14:59:02 -0300

    Add files via upload

---
+++ IDADE.py
@@ -0,0 +1,22 @@
+print('Oi eu sou um programa desenvolvido para saber sua idade')
+print('              Feito por Isaque Alves :D')
+número = int(input('Pense em um número de 2 a 10 (Escreva o número)'))
+x = número * 2
+y = x + 5
+z = y * 50
+print('O programa acabou de realizar um calculo')
+print('Agora ATENÇÃO: Se voce ja fez aniversario em 2021')
+resposta = int(input('Digite 1771, Se não digite 1770. R: '))
+g = z + resposta
+pergunta = int(input('Agora digite sua data de nascimento. R:'))
+q = g - pergunta
+print('Agora preciso da sua ajuda um número foi desenvolvido a partir dos dados')
+print('Esse número que foi gerado tem tres algarismos')
+print('ESQUEÇA O 1 ALGARISMO, SEU NÚMERO É {}'.format(q))
+print('O SEGUNDO E ULTIMO ALGARISMOS')
+print('SAO SUA IDADE')
+print('FIM')
+FIM = input('PRESSIONE ENTER PARA TERMINAR')
+
+
+
