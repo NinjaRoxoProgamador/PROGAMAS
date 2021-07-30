c2fb7ab8b01f1706be234f0aa5f2e9d84b68d46f
NinjaRoxoProgamador <87674724+NinjaRoxoProgamador@users.noreply.github.com>
2021-07-30 14:56:23 -0300

    Add files via upload

---
+++ TABUADA.py
@@ -0,0 +1,27 @@
+h = int(input('Qual e o número?:'))
+print('O codigo do FIM e 20')
+n1 = h * 1
+n2 = h * 2
+n3 = h * 3
+n4 = h * 4
+n5 = h * 5
+n6 = h * 6
+n7 = h * 7
+n8 = h * 8
+n9 = h * 9
+n10 = h *10
+while True:
+ print(f'  {h} * 1 = {n1}')
+ print(f'  {h} * 2 = {n2}')
+ print(f'  {h} * 3 = {n3}')
+ print(f'  {h} * 4 = {n4}')
+ print(f'  {h} * 5 = {n5}')
+ print(f'  {h} * 6 = {n6}')
+ print(f'  {h} * 7 = {n7}')
+ print(f'  {h} * 8 = {n8}')
+ print(f'  {h} * 9 = {n9}')
+ print(f'  {h} * 10 = {n10}')
+ j = int(input('Escreva o Codigo de FIM para terminar '))
+ if j == 20:
+    break
+print('PROGAMA ENCERRADO')
